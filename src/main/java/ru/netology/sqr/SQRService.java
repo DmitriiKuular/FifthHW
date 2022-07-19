cd9dccaf2e9508cf59f1d9de946103125c1384d1
Дмитрий Куулар <kuulardmo@bk.ru>
2022-07-19 18:54:36 +0700

    Changed code

--- src/main/java/ru/netology/sqr/SQRService.java
+++ src/main/java/ru/netology/sqr/SQRService.java
@@ -4,12 +4,20 @@ public class SQRService {
 
     public int calcSqr(int x, int y) {
         int counter = 0;
+//        for (int i = x; i <= y; i++) {
+//            double q = Math.sqrt(i);
+//            if (q % 1 == 0) {
+//                counter++;
+//            }
+
         for (int i = x; i <= y; i++) {
-            double q = Math.sqrt(i);
-            if (q % 1 == 0) {
-                counter++;
+            for (int n = 1; n <= 1_000_000; n++) {
+                if (n * n == i) {
+                    counter++;
+                }
             }
         }
         return counter;
     }
 }
+
